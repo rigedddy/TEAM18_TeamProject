301c31f97f8ac3607408780737f91a11b5dfbd57
Taha-Sid <Tahasid.uk@gmail.com>
2025-03-25 17:20:25 +0000

    - Modified LoginController to validate user login using MySQL database. - Added 'username' and 'password' TextField inputs to interact with the FXML. - Implemented userExists() method to check credentials against LoginDetails table. - Display appropriate messages for successful and failed login attempts.

--- src/main/java/com/example/myjavafx/LoginController.java
+++ src/main/java/com/example/myjavafx/LoginController.java
@@ -2,19 +2,54 @@ package com.example.myjavafx;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
+import java.sql.*;
 
 public class LoginController {
 
     private ActionEvent event;
 
     @FXML
+    private TextField username; // Reference to the TextField in FXML
+    @FXML
+    private TextField password; // Reference to the TextField in FXML
+
+    @FXML
     void signIn(ActionEvent event) throws IOException {
         this.event = event;
-        LoginApplication.moveToDashboard();
+
+        String enteredUserName = username.getText(); // Get user input
+        String enteredPassword = password.getText(); // Get user input
+
+        if (userExists(enteredUserName, enteredPassword)) {
+            System.out.println("Login successful!"); // Debugging message
+            LoginApplication.moveToDashboard(); // Change screen
+        }
+        else {
+            System.out.println("Incorrect username! Try again."); // Debugging message
+        }
     }
 
+    boolean userExists(String username, String password) {
+        try {
+            String query = "SELECT username,password FROM LoginDetails WHERE username = ? and password = ?";
+            PreparedStatement statement = DatabaseConnection.getConnection().prepareStatement(query);
+            statement.setString(1, username);
+            statement.setString(2, password);
+            ResultSet resultSet = statement.executeQuery();
 
+            int c = 0;
+            while (resultSet.next()) {
+                c++;
+//                System.out.println(resultSet.getString(1) + " " + resultSet.getString(2));
+            }
+            return c > 0;
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+        return false;
+    }
 }
